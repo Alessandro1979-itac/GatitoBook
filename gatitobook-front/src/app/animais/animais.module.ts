0ff38d0be34db453d55ee7ff948e57676f73a6ea
alemcar <sandromuniz27@hotmail.com>
2023-02-16 16:09:45 -0400

    Detalhes do animal

--- gatitobook-front/src/app/animais/animais.module.ts
+++ gatitobook-front/src/app/animais/animais.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { CartaoModule } from '../components/cartao/cartao.module';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { AnimalComponent } from './animal/animal.component';
-import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+  ],
   imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
 export class AnimaisModule {}
